69aa7e9979c694c04d07527338514983b270c909
zerocsss <1756297001@qq.com>
2021-05-25 14:59:35 +0800

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,193 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import logo from "./logo.svg";
+import type { ProFormColumnsType, ProFormLayoutType } from '@ant-design/pro-form';
+import ProForm, { ProFormText, BetaSchemaForm, ProFormSelect } from "@ant-design/pro-form";
+import "./App.css";
+
+import { Logo } from "./Logo";
+
+const valueEnum = {
+  all: { text: '全部', status: 'Default' },
+  open: {
+    text: '未解决',
+    status: 'Error',
+  },
+  closed: {
+    text: '已解决',
+    status: 'Success',
+    disabled: true,
+  },
+  processing: {
+    text: '解决中',
+    status: 'Processing',
+  },
+};
+
+type DataItem = {
+  name: string;
+  state: string;
+};
+
+const columns: ProFormColumnsType<DataItem>[] = [
+  {
+    title: '标题',
+    dataIndex: 'title',
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
+    width: 'm',
+  },
+  {
+    title: '状态',
+    dataIndex: 'state',
+    valueType: 'select',
+    valueEnum,
+    width: 'm',
+  },
+  {
+    title: '标签',
+    dataIndex: 'labels',
+    width: 'm',
+  },
+  {
+    title: '创建时间',
+    key: 'showTime',
+    dataIndex: 'createName',
+    valueType: 'date',
+  },
+  {
+    title: '分组',
+    valueType: 'group',
+    columns: [
+      {
+        title: '状态',
+        dataIndex: 'groupState',
+        valueType: 'select',
+        width: 'xs',
+        valueEnum,
+      },
+      {
+        title: '标题',
+        width: 'md',
+        dataIndex: 'groupTitle',
+        formItemProps: {
+          rules: [
+            {
+              required: true,
+              message: '此项为必填项',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  {
+    title: '列表',
+    valueType: 'formList',
+    dataIndex: 'list',
+    initialValue: [{ state: 'all', title: '标题' }],
+    columns: [
+      {
+        valueType: 'group',
+        columns: [
+          {
+            title: '状态',
+            dataIndex: 'state',
+            valueType: 'select',
+            width: 'xs',
+            valueEnum,
+          },
+          {
+            title: '标题',
+            dataIndex: 'title',
+            formItemProps: {
+              rules: [
+                {
+                  required: true,
+                  message: '此项为必填项',
+                },
+              ],
+            },
+            width: 'm',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'FormSet',
+    valueType: 'formSet',
+    dataIndex: 'formSet',
+    columns: [
+      {
+        title: '状态',
+        dataIndex: 'groupState',
+        valueType: 'select',
+        width: 'xs',
+        valueEnum,
+      },
+      {
+        title: '标题',
+        dataIndex: 'groupTitle',
+        tip: '标题过长会自动收缩',
+        formItemProps: {
+          rules: [
+            {
+              required: true,
+              message: '此项为必填项',
+            },
+          ],
+        },
+        width: 'm',
+      },
+    ],
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'created_at',
+    valueType: 'dateRange',
+    transform: (value) => {
+      return {
+        startTime: value[0],
+        endTime: value[1],
+      };
+    },
+  },
+];
 
 function App() {
+  const [layoutType, setLayoutType] = useState<ProFormLayoutType>('Form');
   return (
     <div className="App">
+      <ProFormSelect
+        options={['ProForm', 'ModalForm', 'DrawerForm', 'LightFilter', 'QueryFilter']}
+        fieldProps={{
+          value: layoutType,
+          onChange: (e) => setLayoutType(e),
+        }}
+      />
+      <BetaSchemaForm<DataItem>
+        trigger={<a>点击我</a>}
+        layoutType={layoutType}
+        onFinish={async (values) => {
+          console.log(values);
+        }}
+        columns={columns}
+      />
+      {/* <ProForm
+        onFinish={async (values) => {
+          console.log(values);
+        }}
+      >
+        <ProFormText name="name" label="姓名" />
+      </ProForm> */}
       <header className="App-header">
+        {/* <Logo logo={logo} className="App-logo"></Logo> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/TodoList.tsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+type Props = {
+  handleSubmit: (value: string) => void;
+}
+
+interface State {
+  itemText: string;
+}
+
+class TodoList extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      itemText: "",
+    };
+  }
+
+  private inputRef = React.createRef<HTMLInputElement>();
+
+  private updateValue(value: string) {
+    this.setState({ itemText: value });
+  }
+
+  handleSubmit (e: React.FormEvent<HTMLFormElement>) {
+
+  }
+  
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+
+        </form>
+        <input ref={this.inputRef} className="edit" value={this.state.itemText} />
+      </div>
+    );
+  }
+}
+
+export default TodoList;
