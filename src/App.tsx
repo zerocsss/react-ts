3ebdd780ad9e1ab0c5caeba1b1a8f790da5d2e78
zerocsss <1756297001@qq.com>
2021-05-25 15:19:46 +0800

    test hus

--- src/App.tsx
+++ src/App.tsx
@@ -171,8 +171,7 @@ function App() {
           onChange: (e) => setLayoutType(e),
         }}
       />
-      <BetaSchemaForm<DataItem>
-        trigger={<a>点击我</a>}
+      <BetaSchemaForm<DataItem>trigger={<a>点击我</a>}
         layoutType={layoutType}
         onFinish={async (values) => {
           console.log(values);
